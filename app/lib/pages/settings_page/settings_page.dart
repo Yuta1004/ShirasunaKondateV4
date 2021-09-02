9274363ebb06e05e9b328f482f7f0972d922ba42
NakagamiYuta <guguru0014@gmail.com>
2021-09-02 23:01:15 +0900

    [add](app) 設定画面UIを作った

--- app/lib/pages/settings_page/settings_page.dart
+++ app/lib/pages/settings_page/settings_page.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:settings_ui/settings_ui.dart';
 
 class SettingsPage extends StatefulWidget {
     @override
@@ -15,7 +16,47 @@ class _SettingsPageState extends State<SettingsPage> {
                     style: TextStyle(fontWeight: FontWeight.bold)
                 ),
             ),
-            body: Text("設定画面"),
+            body: Column(
+                children: <Widget>[
+                    SettingsSection(
+                        title: "起動時に翌日の献立を表示",
+                        tiles: <SettingsTile>[
+                            SettingsTile.switchTile(
+                                title: "有効にする",
+                                switchValue: true,
+                                onToggle: (bool value) {},
+                            ),
+                            SettingsTile(
+                                title: "時刻",
+                                subtitle: "00:00から",
+                            ),
+                        ],
+                    ),
+                    SettingsSection(
+                        title: "栄養情報表示",
+                        tiles: <SettingsTile>[
+                            SettingsTile.switchTile(
+                                title: "有効にする",
+                                switchValue: true,
+                                onToggle: (bool value) {},
+                            ),
+                            SettingsTile(
+                                title: "表示する栄養情報",
+                                subtitle: "カロリー/塩分",
+                            ),
+                        ],
+                    ),
+                    SettingsSection(
+                        title: "その他",
+                        tiles: <SettingsTile>[
+                            SettingsTile(
+                                title: "配信サーバURL",
+                                subtitle: "https://www.aaa.example.com",
+                            ),
+                        ],
+                    ),
+                ],
+            ),
         );
     }
 }

d52f4a7c5b9d3f7b3e65578426f32d22ba5aa81a
NakagamiYuta <guguru0014@gmail.com>
2021-09-02 16:40:55 +0900

    [clean](app) stub生成処理を独立させた

--- app/lib/grpc/conn.dart
+++ app/lib/grpc/conn.dart
@@ -4,26 +4,36 @@ import "/grpc/v1/info_distributor_v1.pbgrpc.dart";
 import "/db/manage.dart";
 import "/db/model.dart";
 
+const _ADDRESS = "localhost";
+const _PORT = 50000;
+const _DEV_ENV = true;
+
+KInfoDistributorV1Client createStub() {
+    final options;
+    if(_DEV_ENV) {
+        options = ChannelOptions(credentials: ChannelCredentials.insecure());
+    } else {
+        options = ChannelOptions(credentials: ChannelCredentials.secure());
+    }
+    return KInfoDistributorV1Client(
+        ClientChannel(
+            _ADDRESS,
+            port: _PORT,
+            options: options,
+        ),
+        options: CallOptions(
+            timeout: Duration(seconds: 20),
+        ),
+    );
+}
+
 Future<List<KondateData>> getKondateData(DateTime date) async {
     final exists = await existsKondateData(date);
     if(exists){
         return await selectKondateData(date);
     }
 
-    final channel = ClientChannel(
-        "localhost",
-        port: 50000,
-        options: ChannelOptions(
-            credentials: ChannelCredentials.insecure()      // for Dev
-        )
-    );
-    final stub = KInfoDistributorV1Client(
-        channel,
-        options: CallOptions(
-            timeout: Duration(seconds: 20)
-        )
-    );
-
+    final stub = createStub();
     try {
         final data = await stub.get(Date(year: date.year, month: date.month, dayofmonth: date.day));
         if(data.result == Result.SUCCESS) {

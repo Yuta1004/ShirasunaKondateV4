d4eecf11b24efb473118c3a1ac3af6f3f4ff2215
NakagamiYuta <guguru0014@gmail.com>
2021-09-02 15:02:34 +0900

    [fix](app) 「昨日/今日/明日の献立」文字列が適切に生成されないバグを修正

--- app/lib/utils/date.dart
+++ app/lib/utils/date.dart
@@ -3,15 +3,19 @@ import 'package:intl/intl.dart';
 String genAppropirateDateText(DateTime date) {
     final nowDate = DateTime.now();
     var dateText = DateFormat("M月d日 (W)").format(date);
-    if(date.day == nowDate.add(Duration(days: -1)).day) {
+    if(_equalDate(date, nowDate.add(Duration(days: -1)))) {
         dateText = "昨日の献立";
-    } else if(date.day == nowDate.add(Duration(days: 1)).day) {
+    } else if(_equalDate(date, nowDate.add(Duration(days: 1)))) {
         dateText = "明日の献立";
-    } else if(date.day == nowDate.day) {
+    } else if(_equalDate(date, nowDate)) {
         dateText = "今日の献立";
     } else if(date.year != nowDate.year) {
         dateText = DateFormat("y年M月d日 (W)").format(date);
     }
     dateText = dateText.replaceAll("W", <String>["", "月", "火", "水", "木", "金", "土", "日"][date.weekday]);
     return dateText;
+}
+
+bool _equalDate(DateTime a0, DateTime a1) {
+    return a0.year == a1.year && a0.month == a1.month && a0.day == a1.day;
 }

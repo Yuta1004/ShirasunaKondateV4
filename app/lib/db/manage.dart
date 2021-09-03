805e2285f74ebeede99bf87cd3b9cffd9a407a21
NakagamiYuta <guguru0014@gmail.com>
2021-09-03 23:20:01 +0900

    [fix](app) DB初期化時の処理に落ちるバグがあったので修正

--- app/lib/db/manage.dart
+++ app/lib/db/manage.dart
@@ -1,32 +1,22 @@
 import "package:sqflite/sqflite.dart";
 import "/db/model.dart";
 
-const DBNAME = "kondate.db";
-
-Future<bool> existsTables() async {
-    final conn = await openDatabase(DBNAME);
-    final exists = 1 == Sqflite.firstIntValue(
-        await conn.query(
-            "sqlite_master",
-            columns: ["count(*)"],
-            where: "type = ? and name = ?",
-            whereArgs: ["table", "kondate"]
-        )
+Future<Database> _openDatabase() async {
+    return await openDatabase(
+        "kondateapp.db",
+        version: 1,
+        onCreate: (Database conn, _) async {
+            await conn.execute("create table kondate (date interger, type interger, menu_list text not null, nutritive_list text not null, primary key(date, type))");
+            await conn.execute("create table type (type interger primary key, type_name char[10])");
+            await conn.transaction((txn) async {
+                await txn.execute("insert into type values (1, \"breakfast\"), (2, \"lunch\"), (3, \"dinner\")");
+            });
+        }
     );
-    await conn.close();
-    return exists;
-}
-
-Future<Null> createTables() async {
-    final conn = await openDatabase(DBNAME);
-    await conn.execute("create table kondate (date interger, type interger, menu_list text not null, nutritive_list text not null, primary key(date, type))");
-    await conn.execute("create table type (type interger primary key, type_name char[10])");
-    await conn.execute("insert into type values (1, \"breakfast\"), (2, \"lunch\"), (3, \"dinner\")");
-    await conn.close();
 }
 
 Future<bool> existsKondateData(DateTime date) async {
-    final conn = await openDatabase(DBNAME);
+    final conn = await _openDatabase();
     final exists = 0 < Sqflite.firstIntValue(
         await conn.query(
             "kondate",
@@ -40,7 +30,7 @@ Future<bool> existsKondateData(DateTime date) async {
 }
 
 Future<Null> insertKondateData(KondateData data) async {
-    final conn = await openDatabase(DBNAME);
+    final conn = await _openDatabase();
     await conn.transaction((txn) async {
         await txn.insert("kondate", data.toMap());
     });
@@ -48,7 +38,7 @@ Future<Null> insertKondateData(KondateData data) async {
 }
 
 Future<List<KondateData>> selectKondateData(DateTime date) async {
-    final conn = await openDatabase(DBNAME);
+    final conn = await _openDatabase();
     final List<Map<String, dynamic>> result = await conn.query(
         "kondate",
         columns: ["*"],

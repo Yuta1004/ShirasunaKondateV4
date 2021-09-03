ea413cde2152f1187a2d5ad65960dd5e6b8cbaba
NakagamiYuta <guguru0014@gmail.com>
2021-09-03 14:46:57 +0900

    [remove](app) 時刻設定で別画面に飛ぶようにしていたが変更，削除

--- app/lib/app.dart
+++ app/lib/app.dart
@@ -3,7 +3,6 @@ import "package:flutter/material.dart";
 import "pages/home_page/home_page.dart";
 import "pages/search_page/search_page.dart";
 import "pages/settings_page/settings_page.dart";
-import "pages/settings_page/set_tomorrow_kondate_date_page.dart";
 
 class MyApp extends StatelessWidget {
     @override
@@ -17,7 +16,6 @@ class MyApp extends StatelessWidget {
                 "/": (context) => HomePage(),
                 "/search": (context) => SearchPage(),
                 "/settings": (context) => SettingsPage(),
-                "/settings/set_tomorrow_kondate_date": (context) => SetTomorrowKondateDatePage(),
             },
         );
     }
